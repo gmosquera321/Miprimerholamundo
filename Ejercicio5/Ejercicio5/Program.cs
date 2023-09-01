590e8609358b0de056c910358520bcffeab28b42
Gisella Hurtado Mosquera <ghurtadomosquera504@gmail.com>
2023-09-01 15:44:18 -0500

    ejercicio 1 de logica

--- Ejercicio5/Ejercicio5/Program.cs
+++ Ejercicio5/Ejercicio5/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 Console.WriteLine("Hello, World!");
+namespace conversionGrados
+{
+  class program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ingrese el valor en grados fahrenheit");
+            double fahrenheit = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("la temperatura en grados celsius es:");
+        }
+    }
+
+}     
+         
+ 
+
