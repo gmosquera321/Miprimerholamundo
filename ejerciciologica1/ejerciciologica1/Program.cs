60d0abb122d20e679c315e905b64b9233c103277
Gisella Hurtado Mosquera <ghurtadomosquera504@gmail.com>
2023-09-01 16:06:20 -0500

    Ejercicios logica

--- ejerciciologica1/ejerciciologica1/Program.cs
+++ ejerciciologica1/ejerciciologica1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Lea  las coordenadas de dos puntos y que muestre las distancias entre los puntos.
+
+public class Ejercicio1
+{
+    static void Main(string[] args)
+    {
+        double coordenadas1, coordenadas2;
+        Console.Write("ingrese la coordenada x del primer punto:");
+        coordenadas1 = double.Parse(Console.ReadLine());
+        Console.Write("ingrese la coordenada y del primer punto:");
+        coordenadas1 = double.Parse(Console.ReadLine());
+        Console.Write("ingrese la coordenada x del segundo punto:");
+        coordenadas2 = double.Parse(Console.ReadLine());
+        Console.Write("ingrese la coordenada y del segundo punto:");
+        coordenadas2 = double.Parse(Console.ReadLine());
+
+        double distancia = Math.Sqrt(d: coordenadas2 - coordenadas1 * (coordenadas1 - coordenadas2));
+        Console.WriteLine($"la distancia entre los dos puntos es:{distancia}");
+    }
+}
