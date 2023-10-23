c7379e71df166f62741a93791c591e25c6d8d807
Gisella Hurtado Mosquera <ghurtadomosquera504@gmail.com>
2023-10-22 21:16:57 -0500

    Creacion de la primera clase

---
+++ Ejercicio17/ejercicio17/ejercicio17/Clases/Employe.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio17.Clases
+{
+    public class Employe
+    {
+        public int Id {get;set;}
+        
+        public string FirstName { get;set;}
+
+        public string LastName { get;set;}
+
+        public DateTime BirthDate { get;set;}
+
+        public DateTime HiringDate { get;set;}
+
+        public DateTime IsActive { get;set;}
+
+
+        public decimal
+
+
+    } 
+}
